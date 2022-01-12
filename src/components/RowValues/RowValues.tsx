f949b2bfad5ae2cd700807fa356fc802f202367c
Emmanuel <82957750+emmyduruc@users.noreply.github.com>
2022-01-12 01:48:17 +0100

    fix: updt on validation

--- src/components/RowValues/RowValues.tsx
+++ src/components/RowValues/RowValues.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { mockdbData } from "../../assets/mockdbData";
 import { currencyFormatter } from "../../constant/validation/validation";
 import "./style.scss";
@@ -7,41 +7,39 @@ type Props = {
   id: string;
   name: string;
   checkId: string;
-  handleOnchange:()=>string
 };
 
 const RowValues = ({ rowChannel }: any) => {
   const [inputEvent, setInputEvent] = useState("");
-  console.log(inputEvent);
+ 
 
-  let myArray = [];
-
+let myArray = []
 
   const handleOnchange = (e: any) => {
-    if (inputEvent === undefined || null) {
-      return currencyFormatter(inputEvent);
-    } else {
-      setInputEvent(e.target.value)
-    }
+    setInputEvent(e.target.value)
   };
-
-  useEffect(()=> {
-    currencyFormatter(inputEvent)
-  }, [inputEvent])
-   currencyFormatter(inputEvent);
-
+ 
+  const handleClick = ()=>{
+   let numUSD = Intl.NumberFormat("en-US",{style:"currency",currency:"USD"})
+   const vs = Number(inputEvent)
+   return numUSD.format(vs)
+  }
+  console.log("numUSD",handleClick())
+  //currencyFormatter(inputEvent)
+  
   return (
     <div>
-      <h2>{inputEvent}</h2>
+       <h2>{inputEvent}</h2>
       <div className="individual-values-row" aria-label="individual-values">
         <h4 className="channel-name">{rowChannel.name} </h4>
         <input
           type="text"
-          placeholder="USD"
+          placeholder={`${handleClick()}`}
           value={inputEvent}
-          onChange={handleOnchange}
+          onChange={ (e)=>setInputEvent(e.target.value)}
           className="sea-input"
         />
+          <button onClick={handleClick}>save</button>
         <div>
           <input
             type="radio"
@@ -49,6 +47,7 @@ const RowValues = ({ rowChannel }: any) => {
             name="scales"
             className="checkingbox"
           />
+       
         </div>
         <input type="radio" id="scales" name="scales" className="checkingbox" />
       </div>
